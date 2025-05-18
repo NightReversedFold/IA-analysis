a6c047dffab2604fbab13c30b21c9d670b320fb6
dotrdp <rckydp1@gmail.com>
2025-05-18 04:33:23 -0600

    Added labels

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,26 @@
 
-import torch
-from PIL import Image
-
-
 from AIhandler import AIhandler
-a = AIhandler()
+handler = AIhandler()
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+import PIL
+from pathlib import Path
 
-img = Image.open("prueba.jpeg")
-img = img.convert("RGB")
-codebooks1 = a.GenerateImageCodebooks(image=img, tensor=False)
-codebooks2 = a.GenerateSeqCodebooks("perro blanco")
-difrencia = a.FindDifferenceBetweenCodebooks(codebooks1, codebooks2)
-print("Diferencia entre codebooks: ", difrencia)
+app = FastAPI()
+
+class LabelQuery(BaseModel):
+    classes: list
+    queryFORAI: str
+
+@app.post("/query/")
+async def queryAI(label_query: LabelQuery):
+    print(label_query)
+
+@app.get("/{image_id}")
+async def read_item(image_filename: str):
+    path = Path("data/images/"+image_filename+".jpg")
+    if not path.exists():
+        return {"error": "Item not found"}
+    image = PIL.Image.open(path)
+    return FileResponse(image)
